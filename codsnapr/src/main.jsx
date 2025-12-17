262607f0d5a331f9856ed4df3731007381c225e8
Rocky <72786003+rocky2210@users.noreply.github.com>
2025-12-17 09:11:08 +0530

    Updated main.jsx router

--- codsnapr/src/main.jsx
+++ codsnapr/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -10,8 +10,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/codsnapr">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
